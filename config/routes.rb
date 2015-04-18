d9232b8ad7b29b675c1502119793a3d3f7b3996c
Ewa Galaszewicz <ewagalaszewicz@Ewas-MacBook-Air.local>
2015-04-18 12:41:01 -0700

    Change in views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :adventures
+  get 'bucketlist/:id' =>'adventures#show'
+
+
+  root 'adventures#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
